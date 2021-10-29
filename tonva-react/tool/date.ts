655e58eb18eb01208f2d6957c301efd650bee993
henry <liaohengyi@gmail.com>
2021-10-29 11:27:51 -0400

    dateFromMinuteId

--- tonva-react/tool/date.ts
+++ tonva-react/tool/date.ts
@@ -14,6 +14,7 @@ export function toLocaleDateString(date:Date) {
 
 export const minute2020_01_01 = 26297280;  // 2020-1-1 到 1970-1-1 的毫秒数
 export function dateFromMinuteId(id: number, timeZone?: number): Date {
-	let m = (id / Math.pow(2, 20)) + (timeZone??0) * 60;
+	let envTimezone = env.timeZone;
+	let m = (id / Math.pow(2, 20)) + (timeZone??envTimezone - envTimezone) * 60;
 	return new Date((m + minute2020_01_01) * 60000);
 }
